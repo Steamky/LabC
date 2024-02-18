0532ff7a4e649ae83aa9233fc542f145e0a9fd54
Steamky <oegorych97@mail.ru>
2024-02-18 18:00:12 +1000

    Update

---
+++ index.js
@@ -0,0 +1,9 @@
+Balance = prompt("Введите баланс вашего счёта:");
+var mobile = 0
+cost_smartphone = 200
+while (Balance >= 0) {
+    Balance = Balance - cost_smartphone;
+    mobile=+1
+    
+}
+console.log(mobile) 
